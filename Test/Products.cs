6d7bf488c31695e9a9859395b60797ef80dbad59
Michał Ziober <michalziober08@gmail.com>
2022-12-08 00:04:07 +0100

    small changes

--- Test/Products.cs
+++ Test/Products.cs
@@ -1,52 +1,52 @@
 using Shared;
-using Repo.DataAccess;
+
 public class Products
 {
     
     [Test]
     public void createProducts()
     {
-        Offer offer = new Offer();
-        offer.AddToOffer(new Product("Szlifierka", 300, Category.Narzędzia, "Szlifierka o wadze 2kg"));
-        offer.AddToOffer(new Product("Młotek", 35, Category.Narzędzia, "Waga główki 0.4kg"));
-        offer.AddToOffer(new Product("Wiertarka", 260, Category.Narzędzia, "Obrotów na minutę: 700"));
-        offer.AddToOffer(new Product("Obcęgi", 15, Category.Narzędzia, "Waga całkowita: 150g"));
-        offer.AddToOffer(new Product("Kombinerki", 18, Category.Narzędzia, "Materiał: guma, stal"));
-        offer.AddToOffer(new Product("Śrubokręt", 13, Category.Narzędzia, "Typ: Płaski"));
-        offer.AddToOffer(new Product("Piła mechaniczna", 444, Category.Narzędzia, "Długość prowadnicy: 0.8m"));
-        offer.AddToOffer(new Product("Sprężarka", 550, Category.Narzędzia, "Kolor: żółty"));
-        offer.AddToOffer(new Product("Spawarka", 499, Category.Narzędzia, "Waga: 40kg"));
-        offer.AddToOffer(new Product("Pilarka", 300, Category.Narzędzia, "Producent: Tregeret"));
-        offer.AddToOffer(new Product("Pralka", 2300, Category.AGD, "Max ładowność: 5kg"));
-        offer.AddToOffer(new Product("Kuchenka Mikrofalowa", 350, Category.AGD, "Tryb rozmrażania, Low, High"));
-        offer.AddToOffer(new Product("Zmywarka", 2150, Category.AGD, "Ilość półek: 2"));
-        offer.AddToOffer(new Product("Piekarnik", 1300, Category.AGD, "Max temperatura: 240 stopni Celsjusza"));
-        offer.AddToOffer(new Product("Okap", 405, Category.AGD, "Głośność pracy: 35dB"));
-        offer.AddToOffer(new Product("Płyta indukcyjna", 1340, Category.AGD, "Ilość palników: 4"));
-        offer.AddToOffer(new Product("Płyta gazowa", 800, Category.AGD, "Zastosowanie: Kuchnia"));
-        offer.AddToOffer(new Product("Lodówka", 2700, Category.AGD, "Minimalna temperatura: 1 stopień Celsjusza"));
-        offer.AddToOffer(new Product("Klimatyzator", 2600, Category.AGD, "Wymiary: 80cmx50cmx40cm"));
-        offer.AddToOffer(new Product("Odkurzacz", 470, Category.AGD, "Możliwość regulacji mocy"));
-        offer.AddToOffer(new Product("Rezystor 20 ohm", 0.5, Category.Elektrotechnika, "Mały, twardy, kwaśny"));
-        offer.AddToOffer(new Product("Oscyloskop", 800, Category.Elektrotechnika, "Nie lizać ekranu"));
-        offer.AddToOffer(new Product("Tranzystor", 0.7, Category.Elektrotechnika, "Wyprowadzenia: 3"));
-        offer.AddToOffer(new Product("Generator", 600, Category.Elektrotechnika, "Bardzo mocny, O S T R O Ż N I E"));
-        offer.AddToOffer(new Product("Przewód miedziany 2m", 10, Category.Elektrotechnika, "Rodzaj przewodnika: miedź"));
-        offer.AddToOffer(new Product("Dioda LED", 0.3, Category.Elektrotechnika, "Kolor: żółty"));
-        offer.AddToOffer(new Product("Czujnik temperatury", 5, Category.Elektrotechnika, "Max temperatura: 150 stopni Celsjusza"));
-        offer.AddToOffer(new Product("Przycisk", 2, Category.Elektrotechnika, "PRzycisk przycisk przycisk"));
-        offer.AddToOffer(new Product("Czujnik odległości", 7, Category.Elektrotechnika, "Tolerancja +-10cm"));
-        offer.AddToOffer(new Product("Fotorezystor", 1, Category.Elektrotechnika, "Działa super, polecam"));
-        offer.AddToOffer(new Product("Kask", 80, Category.Ubiór, "Mocny, odporny na spadające cegły"));
-        offer.AddToOffer(new Product("Spodnie ogrodniki", 90, Category.Ubiór, "Wyglądaj jak prawdziwy ogrodnik"));
-        offer.AddToOffer(new Product("Koszula flanelowa", 40, Category.Ubiór, "Kolor: czarny"));
-        offer.AddToOffer(new Product("Rękawice ogrodowe", 7, Category.Ubiór, "Odpowiednie na każde dłonie"));
-        offer.AddToOffer(new Product("Rękawice spawalnicze", 25, Category.Ubiór, "Zapobiega oparzeniom"));
-        offer.AddToOffer(new Product("Fartuch spawalniczy", 160, Category.Ubiór, "Długośc całkowita: 1.3m"));
-        offer.AddToOffer(new Product("Przyłbica", 254, Category.Ubiór, "Automatyczne ściemnianie szybki pod wpływem intensywnego światła"));
-        offer.AddToOffer(new Product("Okulary ochronne", 30, Category.Ubiór, "Ochroń swoje oczy teraz"));
-        offer.AddToOffer(new Product("Kamizelka", 120, Category.Ubiór, "Ciepła, idealna na jesienne prace na zewnątrz"));
-        offer.AddToOffer(new Product("Kurtka przeciwdeszczowa", 170, Category.Ubiór, "Żaden deszcz jej nie straszny!"));
+        // Offer offer = new Offer();
+        // offer.AddToOffer(new Product("Szlifierka", 300, Category.Narzędzia, "Szlifierka o wadze 2kg"));
+        // offer.AddToOffer(new Product("Młotek", 35, Category.Narzędzia, "Waga główki 0.4kg"));
+        // offer.AddToOffer(new Product("Wiertarka", 260, Category.Narzędzia, "Obrotów na minutę: 700"));
+        // offer.AddToOffer(new Product("Obcęgi", 15, Category.Narzędzia, "Waga całkowita: 150g"));
+        // offer.AddToOffer(new Product("Kombinerki", 18, Category.Narzędzia, "Materiał: guma, stal"));
+        // offer.AddToOffer(new Product("Śrubokręt", 13, Category.Narzędzia, "Typ: Płaski"));
+        // offer.AddToOffer(new Product("Piła mechaniczna", 444, Category.Narzędzia, "Długość prowadnicy: 0.8m"));
+        // offer.AddToOffer(new Product("Sprężarka", 550, Category.Narzędzia, "Kolor: żółty"));
+        // offer.AddToOffer(new Product("Spawarka", 499, Category.Narzędzia, "Waga: 40kg"));
+        // offer.AddToOffer(new Product("Pilarka", 300, Category.Narzędzia, "Producent: Tregeret"));
+        // offer.AddToOffer(new Product("Pralka", 2300, Category.AGD, "Max ładowność: 5kg"));
+        // offer.AddToOffer(new Product("Kuchenka Mikrofalowa", 350, Category.AGD, "Tryb rozmrażania, Low, High"));
+        // offer.AddToOffer(new Product("Zmywarka", 2150, Category.AGD, "Ilość półek: 2"));
+        // offer.AddToOffer(new Product("Piekarnik", 1300, Category.AGD, "Max temperatura: 240 stopni Celsjusza"));
+        // offer.AddToOffer(new Product("Okap", 405, Category.AGD, "Głośność pracy: 35dB"));
+        // offer.AddToOffer(new Product("Płyta indukcyjna", 1340, Category.AGD, "Ilość palników: 4"));
+        // offer.AddToOffer(new Product("Płyta gazowa", 800, Category.AGD, "Zastosowanie: Kuchnia"));
+        // offer.AddToOffer(new Product("Lodówka", 2700, Category.AGD, "Minimalna temperatura: 1 stopień Celsjusza"));
+        // offer.AddToOffer(new Product("Klimatyzator", 2600, Category.AGD, "Wymiary: 80cmx50cmx40cm"));
+        // offer.AddToOffer(new Product("Odkurzacz", 470, Category.AGD, "Możliwość regulacji mocy"));
+        // offer.AddToOffer(new Product("Rezystor 20 ohm", 0.5, Category.Elektrotechnika, "Mały, twardy, kwaśny"));
+        // offer.AddToOffer(new Product("Oscyloskop", 800, Category.Elektrotechnika, "Nie lizać ekranu"));
+        // offer.AddToOffer(new Product("Tranzystor", 0.7, Category.Elektrotechnika, "Wyprowadzenia: 3"));
+        // offer.AddToOffer(new Product("Generator", 600, Category.Elektrotechnika, "Bardzo mocny, O S T R O Ż N I E"));
+        // offer.AddToOffer(new Product("Przewód miedziany 2m", 10, Category.Elektrotechnika, "Rodzaj przewodnika: miedź"));
+        // offer.AddToOffer(new Product("Dioda LED", 0.3, Category.Elektrotechnika, "Kolor: żółty"));
+        // offer.AddToOffer(new Product("Czujnik temperatury", 5, Category.Elektrotechnika, "Max temperatura: 150 stopni Celsjusza"));
+        // offer.AddToOffer(new Product("Przycisk", 2, Category.Elektrotechnika, "PRzycisk przycisk przycisk"));
+        // offer.AddToOffer(new Product("Czujnik odległości", 7, Category.Elektrotechnika, "Tolerancja +-10cm"));
+        // offer.AddToOffer(new Product("Fotorezystor", 1, Category.Elektrotechnika, "Działa super, polecam"));
+        // offer.AddToOffer(new Product("Kask", 80, Category.Ubiór, "Mocny, odporny na spadające cegły"));
+        // offer.AddToOffer(new Product("Spodnie ogrodniki", 90, Category.Ubiór, "Wyglądaj jak prawdziwy ogrodnik"));
+        // offer.AddToOffer(new Product("Koszula flanelowa", 40, Category.Ubiór, "Kolor: czarny"));
+        // offer.AddToOffer(new Product("Rękawice ogrodowe", 7, Category.Ubiór, "Odpowiednie na każde dłonie"));
+        // offer.AddToOffer(new Product("Rękawice spawalnicze", 25, Category.Ubiór, "Zapobiega oparzeniom"));
+        // offer.AddToOffer(new Product("Fartuch spawalniczy", 160, Category.Ubiór, "Długośc całkowita: 1.3m"));
+        // offer.AddToOffer(new Product("Przyłbica", 254, Category.Ubiór, "Automatyczne ściemnianie szybki pod wpływem intensywnego światła"));
+        // offer.AddToOffer(new Product("Okulary ochronne", 30, Category.Ubiór, "Ochroń swoje oczy teraz"));
+        // offer.AddToOffer(new Product("Kamizelka", 120, Category.Ubiór, "Ciepła, idealna na jesienne prace na zewnątrz"));
+        // offer.AddToOffer(new Product("Kurtka przeciwdeszczowa", 170, Category.Ubiór, "Żaden deszcz jej nie straszny!"));
         
         // DataAccess da = DataAccess.Instance;
         // da.OfferList = new List<Offer>{offer};
