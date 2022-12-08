4dd90522e2b9a63a695c3a79b018902354d49ee2
Jakub Maciejewski <jakubm55555@gmail.com>
2022-12-08 16:30:17 +0100

    Small hotfix - ClientOperations

--- application/Services/ClientOperations.cs
+++ application/Services/ClientOperations.cs
@@ -8,7 +8,7 @@ public class ClientOperations : IClientOperations
     IRepository repository;
     public ClientOperations()
     {
-        IRepository repository = Repository.Instance;
+        repository = Repository.Instance;
     }
     public bool checkClientCredentials(string login, string password)
     {
