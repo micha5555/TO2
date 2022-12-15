7fc0a69d6f802f6b15ddee7ceee89894f3b841c0
Jakub Maciejewski <jakubm55555@gmail.com>
2022-12-16 00:04:26 +0100

    Created tests for proposing items

--- BDDTests/StepDefinitions/ProposingItemsCartStepDefinitions.cs
+++ BDDTests/StepDefinitions/ProposingItemsCartStepDefinitions.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using Shared;
 
 namespace BDDTests.StepDefinitions;
@@ -5,28 +7,27 @@ namespace BDDTests.StepDefinitions;
 [Binding]
 public class ProposingItemsCartStepDefinitions  
 {
-
-    // [Given(@"Client is logged in")]
-    // public void GivenClientisloggedin()
-    // {
-    // }
-    Cart _cart;
-
-    [Given(@"Clients cart is not empty")]
-    public void GivenClientscartisnotempty()
-    {
-        // _cart = client.Cart;
+    private List<Product> proposedProducts;
+    
+    [Given(@"Other clients orders exist")]
+    public void GivenOtherClientsOrdersExist(){
+        Helper.CommonMethods.CreateGivenNumberOfOrdersForGivenClient(new Client("Janusz", "Tracz", "Warszawa ul. Konopianki", "04-050", "Jantex", "Tralala"), 3);
+        Helper.CommonMethods.CreateGivenNumberOfOrdersForGivenClient(new Client("Mariusz", "Pudzianowski", "Lubicz", "14-200", "BYczunio", "ByCzEk"), 2);
+        Helper.CommonMethods.CreateGivenNumberOfOrdersForGivenClient(new Client("Tomasz", "Hajto", "Gelsenkirchen", "1212-1", "Pasiasty", "SzyBko"), 2,3);
     }
 
     [When(@"Client enters his cart")]
     public void WhenCliententershiscart()
     {
+        proposedProducts = Helper.BaseServices.GeneralOperations.ProposeProductsBasedOnCart(Helper.BaseClient.Client.Cart, 2);
     }
 
     [Then(@"Client get list of proposed products")]
     public void ThenClientgetlistofproposedproducts()
     {
-
+        Assert.AreEqual(2, proposedProducts.Count);
+        Assert.Contains(Helper.CommonMethods.CreateNewCorrectAGDProduct(), proposedProducts);
+        Assert.Contains(Helper.CommonMethods.CreateNewCorrectNarzedziaProduct(), proposedProducts);
     }
 
 }
