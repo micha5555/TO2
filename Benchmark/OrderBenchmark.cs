2d86eabc1bf5b7b9827abad85c7c8e0248237fde
Michał Ziober <michalziober08@gmail.com>
2023-01-13 01:01:07 +0100

    order benchmark

--- Benchmark/OrderBenchmark.cs
+++ Benchmark/OrderBenchmark.cs
@@ -7,27 +7,29 @@ namespace Benchmark;
 [MemoryDiagnoser]
 public class OrderBenchmark{
 
-
-    private List<Product> products = new List<Product>();
+    private List<CartProduct> products = new List<CartProduct>();
     private IOrderOperations _orderOperations = new OrderOperations();
     private IGeneralOperations _generalOperations = new GeneralOperations();
-    private Client client;
-    private Order order;
+    private Client? client;
+    private Order? order;
 
-    [GlobalSetup]
-    public void GlobalSetup(){
-        List<Client> clients = Common.GenerateClients(1);
-        client = clients[0];
-        order = new Order(client);
-    }
+    [Params(1, 10, 20)]
+    public int N;
 
     [IterationSetup]
     public void IterationSetup(){
         _generalOperations.ReadDataOnLaunch();
+        products = Common.GenerateCartProducts(5);
+        List<Client> clients = Common.GenerateClients(1);
+        client = clients[0];
+        foreach(CartProduct cp in products){
+            client.Cart.AddToCart(cp);
+        }
+        order = new Order(client);
     }
 
     [Benchmark]
-    public void CreratingOrder()
+    public void CreatingOrder()
     {
         _orderOperations.CreateOrder(client);
     }
